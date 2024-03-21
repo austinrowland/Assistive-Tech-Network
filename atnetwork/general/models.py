d948ff08ac6084805bfe319cb3c1994296d796a9
averylg <71529955+averylg@users.noreply.github.com>
2024-03-20 23:43:21 -0400

    Migrates models, adds first template

--- atnetwork/general/models.py
+++ atnetwork/general/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model): # User was already registered with admin
+    username = models.CharField(max_length=128)
+    display_name = models.CharField(max_length=128)
+    email = models.EmailField(max_length=200)
+    date_joined = models.DateTimeField("Date joined")
+    # May eventually link to posts that a user has made once we get that model
+
+    def __str__(self):
+        return self.username
+
+class Organization(models.Model):
+    display_name = models.CharField(max_length=128)
+    affiliated_users = models.ManyToManyField(Profile)
+    
+    def __str__(self):
+        return self.display_name
+
+class Product(models.Model):
+    display_name = models.CharField(max_length=200)
+    image = models.ImageField()
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+
+    def __str__(self):
+        return self.display_name
--- atnetwork/general/views.py
+++ atnetwork/general/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Product
 
 def index(request):
-    return HttpResponse("Hello, TOM. You're at the general homepage index.")
+    product_list = Product.objects.order_by('-price')[:5]
+    template = loader.get_template("general/index.html")
+    context = {
+        "product_list": product_list
+    }
+    return HttpResponse(template.render(context, request))
